05b7229e50f98b5970ae0308598876c89a7aa7ef
shyam <shyamgodavari7@gmail.com>
2024-03-19 12:04:36 +0530

    second initilisation

--- src/git/Demo1.java
+++ src/git/Demo1.java
@@ -3,6 +3,7 @@ package git;
 public class Demo1 {
 public static void main(String[] args) {
 	
-	System.out.println("hello world");
+	System.out.println("hello ");
+	System.out.println("hiiiii");
 }
 }
