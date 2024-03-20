f78f131c0bff7d5871694cd25d84fd1c18bc13d6
shyam <shyamgodavari7@gmail.com>
2024-03-20 11:18:33 +0530

    first

--- src/git/Demo1.java
+++ src/git/Demo1.java
@@ -3,7 +3,7 @@ package git;
 public class Demo1 {
 public static void main(String[] args) {
 	
-	System.out.println("hello ");
-	System.out.println("hiiiii");
+	System.out.println("shyams git");
+
 }
 }
